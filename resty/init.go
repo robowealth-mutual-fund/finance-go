6c488c27796cacf47d0d370e8907f393162d8512
psomdul <48184042+psomdul@users.noreply.github.com>
2025-02-18 11:46:48 +0700

    refactor

--- resty/init.go
+++ resty/init.go
@@ -1,10 +1,7 @@
 package resty
 
 import (
-	//"account-service/internals/config"
-	//"account-service/internals/infrastructure/http_client"
 	"github.com/robowealth-mutual-fund/finance-go/http_client"
-	//httpClient "github.com/robowealth-mutual-fund/finance-go/http_client"
 )
 
 type Repository struct {
